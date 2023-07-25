07facea184aee3667fdb04bc5f475d0a5fb8144a
johanespratama9 <johanespratama09@gmail.com>
2023-07-25 13:40:29 +0700

    add repository user

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"bwastartup/user"
+	"log"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func main() {
+	// refer https://github.com/go-sql-driver/mysql#dsn-data-source-name for details
+	dsn := "root:@tcp(127.0.0.1:3306)/bwastartup?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	userRepository := user.NewRepository(db)
+	user := user.User{
+		Name: "test simpan",
+	}
+	userRepository.Save(user)
+}
